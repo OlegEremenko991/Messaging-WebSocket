20be32e97299dbd8d083b2735149256db185eb18
Oleg991 <o.n.eremenko@gmail.com>
2020-07-02 09:45:48 +0300

    Initial Commit

---
+++ Messaging app (WebSocket)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messaging app (WebSocket)
+//
+//  Created by Олег Еременко on 02.07.2020.
+//  Copyright © 2020 Oleg Eremenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
